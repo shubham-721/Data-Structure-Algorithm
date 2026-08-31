cbb7fef0243e081f1c3c1c9bb717e285a101947e
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-08-31 15:43:03 +0530

    implmented kadanes algo

--- Array/MaximumSubArraySum.py
+++ Array/MaximumSubArraySum.py
@@ -1,9 +1,26 @@
 nums=[12,-4,3,23,52,-2,53,5,-4]
 max_sum=nums[0]
+# for i in range(len(nums)):
+#     sum=0
+#     for j in range(i,len(nums)):
+#         sum=sum+nums[j]
+#         if sum>max_sum:   
+#             max_sum=sum
+# print(max_sum)
+sum=0
+ansStart=0
+ansEnd=0
+start=0
 for i in range(len(nums)):
-    sum=0
-    for j in range(i,len(nums)):
-        sum=sum+nums[j]
-        if sum>max_sum:
-            max_sum=sum
+    if sum==0:
+        start=i
+    sum=sum+nums[i]
+    if sum>max_sum:
+        max_sum=sum
+        ansStart=start
+        ansEnd=i
+    if sum<0:
+        sum=0
 print(max_sum)
+print(ansStart,ansEnd)
+
