505d7379b9fc30656e234cf612f0963d5ac6c304
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-08-31 13:50:38 +0530

    moore voting algorithm

--- Array/majorityElement.py
+++ Array/majorityElement.py
@@ -1,4 +1,4 @@
-nums=[2,2,1,1,1,2,2,1,1]
+nums=[2,2,1,1,1,2,2,1,1,2,2]
 n=len(nums)
 # for num in nums:
 #     count=0
@@ -7,12 +7,28 @@ n=len(nums)
 #             count+=1
 # if count>n/2:
 #     print(num)
-count={}
-for num in nums:
-    if num in count: 
-        count[num]=count[num]+1
+# count={}
+# for num in nums:
+#     if num in count: 
+#         count[num]=count[num]+1
+#     else:
+#         count[num]=1
+# for num in count:
+#     count[num]>n//2
+# print(num)
+candidate=nums[0]
+count=1
+for num in nums[1:]:
+    if num ==candidate:
+        count+=1
     else:
-        count[num]=1
-for num in count:
-    count[num]>n//2
-print(num)
+        count-=1
+    if count ==0:
+        candidate=num
+        count=1
+count=0
+for num in nums:
+    if num==candidate:
+        count+=1
+if count>n//2:
+    print(num)
