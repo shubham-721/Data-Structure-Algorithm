c810807f40c901d18a788e66eb881aa230c03de4
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-03 12:04:26 +0530

    optimal apporach

--- Array/ArrayLeader.py
+++ Array/ArrayLeader.py
@@ -1,14 +1,28 @@
 nums=[16,17,4,3,5,2]
 # output=[17,5,2]
 result=[]
-for i in range(len(nums)):
-    leader=True
-    for j in range(i+1,len(nums)):
-        if nums[j]>nums[i]:
-            leader=False
-            break
-    if (leader==True):
-        result.append(nums[i])
+# for i in range(len(nums)):
+#     leader=True
+#     for j in range(i+1,len(nums)):
+#         if nums[j]>nums[i]:
+#             leader=False
+#             break
+#     if (leader==True):
+#         result.append(nums[i])
+# print(result)
+n=len(nums)
+result=[nums[n-1]]
+maxi=nums[n-1]
+for i in range(n-2,-1,-1):
+    if maxi<nums[i]:
+        maxi=nums[i]
+        result.append(maxi)
 print(result)
-
-        
+n=len(result)
+i=0
+j=n-1
+while j<n:
+    result[i],result[j]=result[j],result[i]
+    i+=1
+    j+=1
+print(result)
