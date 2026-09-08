808596a95c5fbe6b1d9144cbea9d77004bd2df99
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-08 22:21:38 +0530

    brute + better approach

--- Array/theLongestsubarraywithsumk.py
+++ Array/theLongestsubarraywithsumk.py
@@ -1,16 +1,26 @@
 nums=[10, 5, 2, 7, 1, 9]
 k=int(input("enter the value of K"))
 n=len(nums)
-max_len=0
-prefix_sum=0
-mp={}
+# max_len=0
+# prefix_sum=0
+# mp={}
+# for i in range(n):
+#     prefix_sum+=nums[i]
+#     if prefix_sum==k:
+#         max_len=i+1
+#     if prefix_sum-k in mp:
+#         length=i-mp[prefix_sum-k]
+#         max_len=max(length,max_len)
+#     if prefix_sum not in mp:
+#         mp[prefix_sum]=i
+# print(max_len)
+length=0
 for i in range(n):
-    prefix_sum+=nums[i]
-    if prefix_sum==k:
-        max_len=i+1
-    if prefix_sum-k in mp:
-        length=i-mp[prefix_sum-k]
-        max_len=max(length,max_len)
-    if prefix_sum not in mp:
-        mp[prefix_sum]=i
-print(max_len)
+    sum=0
+    for j in range(i,n):
+        sum+=nums[j]
+        if (sum==k):
+            current_len=j-i+1
+            if current_len>length:
+                length=current_len
+print(length)
