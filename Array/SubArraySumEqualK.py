36bf33d2dc165970c9c3245213c497e6240182c6
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-09 13:53:39 +0530

    better approach

--- Array/SubArraySumEqualK.py
+++ Array/SubArraySumEqualK.py
@@ -2,11 +2,27 @@ nums=[1,2,3]
 # we have to find the  subarray sum equal k
 n=len(nums)
 k=int(input("enter the number "))
-count=0
+# count=0
+# for i in range(n):
+#     sum=0
+#     for j in range(i,n):
+#         sum=sum+nums[j]
+#         if sum==k:
+#             count+=1        
+# print(count)
+dic={}
+max_len=0
+sum=0
 for i in range(n):
-    sum=0
-    for j in range(i,n):
-        sum=sum+nums[j]
-        if sum==k:
-            count+=1        
-print(count)
+    sum+=nums[i]
+    if sum==k:
+        if max_len<i+i:
+            max_len=i+i
+    rem=sum-k
+    if rem in dic:
+        len=i-dic[rem]
+        if max_len<len:
+            max_len=len
+    if sum not in dic:
+        dic[sum]=i
+print(max_len)
--- Array/theLongestsubarraywithsumk.py
+++ Array/theLongestsubarraywithsumk.py
@@ -1,20 +1,6 @@
 nums=[10, 5, 2, 7, 1, 9]
 k=int(input("enter the value of K"))
 n=len(nums)
-# max_len=0
-# prefix_sum=0
-# mp={}
-# for i in range(n):
-#     prefix_sum+=nums[i]
-#     if prefix_sum==k:
-#         max_len=i+1
-#     if prefix_sum-k in mp:
-#         length=i-mp[prefix_sum-k]
-#         max_len=max(length,max_len)
-#     if prefix_sum not in mp:
-#         mp[prefix_sum]=i
-# print(max_len)
-length=0
 for i in range(n):
     sum=0
     for j in range(i,n):
