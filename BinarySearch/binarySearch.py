ea29a4c1e772eb652084b326c19d146616eb511c
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-13 08:43:52 +0530

    binarySearch

--- BinarySearch/binarySearch.py
+++ BinarySearch/binarySearch.py
@@ -1,1 +1,15 @@
-nums=[2,413,141,41,41]
+def binarySearch(nums,n,target):
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif target>nums[mid]:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+nums=[3,4,6,7,9,12,16,17]
+n=len(nums)
+print(binarySearch(nums,n,target=13))           
