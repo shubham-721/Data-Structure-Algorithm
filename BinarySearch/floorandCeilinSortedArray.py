fb7ffa86016b9b77a941e83b1ee82e6aafd45f27
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-15 14:41:54 +0530

    floor and ceil in a sorted array

---
+++ BinarySearch/floorandCeilinSortedArray.py
@@ -0,0 +1,21 @@
+nums=[1, 2, 4, 6, 8]
+x=5
+n=len(nums)
+floor=-1
+ceil=-1
+low=0
+high=n-1
+while low<=high:
+    mid=low+(high-low)//2
+    if nums[mid]==x:
+        floor = nums[mid]
+        ceil = nums[mid]
+        break
+    elif nums[mid]<x:
+        low=mid+1
+        floor=nums[mid]
+    else:
+        high=mid-1
+        ceil=nums[mid]
+print(floor)
+print(ceil)
