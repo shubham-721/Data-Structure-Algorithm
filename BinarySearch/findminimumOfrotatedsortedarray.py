efbfdc83cb4179c18c91b6e246bab44b11de7e62
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-16 16:51:04 +0530

    minmum of rotated sorted array

---
+++ BinarySearch/findminimumOfrotatedsortedarray.py
@@ -0,0 +1,11 @@
+nums=[3,4,5,1,2]
+n=len(nums)
+low=0
+high=n-1
+while low<high:
+    mid=low+(high-low)//2
+    if nums[mid]>nums[high]:
+        low=mid+1
+    else:
+        high=mid
+print(nums[low])
