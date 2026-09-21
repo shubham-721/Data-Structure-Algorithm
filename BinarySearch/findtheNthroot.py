6b0a24cb957828ebbcd81c858b7700e7b5271ab7
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-21 16:09:03 +0530

    nth using binary search

---
+++ BinarySearch/findtheNthroot.py
@@ -0,0 +1,19 @@
+x=int(input("enter the number"))
+m=int(input("enter the power"))
+low=1
+high=x
+result=-1
+while low<=high:
+    mid=low+(high-low)//2
+    ans= mid**m
+    if ans==x:
+        result=mid
+        break
+    elif ans<x:
+        low=mid+1
+    else:
+        high=mid-1
+print(result)
+
+            
+      
