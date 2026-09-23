a50b4a88a6db487d572255256f7bec4637f5b485
shubham-721 <shubhammadheshiya7111@gmail.com>
2026-09-23 10:16:41 +0530

    optimal approach

--- BinarySearch/findthesamllestdivisorthresoldDivisor.py
+++ BinarySearch/findthesamllestdivisorthresoldDivisor.py
@@ -1,12 +1,17 @@
 nums=[1,2,5,9]
 t=6
-result=0
 # output=5
-large=0
-for i in range(len(nums)):
-    if nums[i]>large:
-        large=nums[i]
-print(large)
-for i in range(1,large+1):
-    total=0
+low=1
+high=max(nums)
+while low<=high:
+    mid=low+(high-low)//2
+    div=0
+    for d in nums:
+        div=div+(d+mid-1)//mid
+    if div<=t:
+        high=mid-1
+    else:
+        low=mid+1
+print(low)
+
     
